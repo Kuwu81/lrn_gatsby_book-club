0edf48033063e06b6fd3a23035a60cb1c6698bb3
Anna Klimenko <anna.klimenko@pwc.com>
2021-05-25 14:48:44 +0200

    added registration in firebase

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,8 +1,10 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import {FirebaseContext} from '../components/Firebase';
 import {Form, Input, Button} from "../components/common";
 
 
 const Register = () => {
+    const { firebase } = useContext(FirebaseContext);
     const [formValues, setFormValues] = useState({
         email: '',
         password: '',
@@ -18,7 +20,12 @@ const Register = () => {
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(formValues);
+        if(formValues.password === formValues.confirmPassword) {
+            firebase.register({
+                email: formValues.email,
+                password: formValues.password
+            });
+        }
     }
     return <Form onSubmit={handleSubmit}>
         <Input name="email" value={formValues.email} onChange={handleInputChange} placeholder="email" type="email" required />

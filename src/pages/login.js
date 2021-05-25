ab102ae056fc9afc02a3424ac41b9c1f1815e3a7
Anna Klimenko <anna.klimenko@pwc.com>
2021-05-25 15:01:39 +0200

    added validation for login & register

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,18 +1,22 @@
 import React, { useState, useContext } from "react";
 import { FirebaseContext } from '../components/Firebase';
-import {Form, Input, Button} from "../components/common";
+import {Form, Input, Button, ErrorMessage} from "../components/common";
 
 const LoginPage = () => {
     const { firebase } = useContext(FirebaseContext);
     const [formValues, setFormValues] = useState({ email: '', password: ''});
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleSubmit(e) {
         e.preventDefault();
-        firebase.login(formValues);
+        firebase.login(formValues).catch((error) => {
+            setErrorMessage(error.message);
+        });
     }
 
     function handleInputChange(e) {
         e.persist();
+        setErrorMessage('');
         setFormValues((currentValues) => ({
             ...currentValues,
              [e.target.name]: e.target.value
@@ -23,6 +27,11 @@ const LoginPage = () => {
             <Form onSubmit={handleSubmit}>
                 <Input name="email" value={formValues.email} placeholder="email" type="email" onChange={handleInputChange} />
                 <Input name="password" value={formValues.password } placeholder="password" type="password" onChange={handleInputChange} />
+                { !!errorMessage &&
+                    <ErrorMessage>
+                        {errorMessage}
+                    </ErrorMessage>
+                }
                 <Button type="submit" block={true}>Login</Button>
             </Form>
         </section>);
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,10 +1,11 @@
 import React, {useState, useContext} from "react";
 import {FirebaseContext} from '../components/Firebase';
-import {Form, Input, Button} from "../components/common";
+import {Form, Input, Button, ErrorMessage} from "../components/common";
 
 
 const Register = () => {
     const { firebase } = useContext(FirebaseContext);
+    const [errorMessage, setErrorMessage] = useState('');
     const [formValues, setFormValues] = useState({
         email: '',
         password: '',
@@ -12,6 +13,7 @@ const Register = () => {
     });
 
     function handleInputChange(e) {
+        setErrorMessage('');
         setFormValues((currentValues) => ( {
             ...currentValues,
             [e.target.name]: e.target.value
@@ -24,13 +26,20 @@ const Register = () => {
             firebase.register({
                 email: formValues.email,
                 password: formValues.password
-            });
+            }).catch((error) => setErrorMessage(error.message));
+        } else {
+            setErrorMessage('Password and Confirm password fields must match');
         }
     }
     return <Form onSubmit={handleSubmit}>
         <Input name="email" value={formValues.email} onChange={handleInputChange} placeholder="email" type="email" required />
-        <Input name="password" value={formValues.password} onChange={handleInputChange} placeholder="password" type="password" required minLength={3} />
-        <Input name="confirmPassword" value={formValues.confirmPassword} onChange={handleInputChange} placeholder="confirm password" type="password" required minLength={3} />
+        <Input name="password" value={formValues.password} onChange={handleInputChange} placeholder="password" type="password" required minLength={6} />
+        <Input name="confirmPassword" value={formValues.confirmPassword} onChange={handleInputChange} placeholder="confirm password" type="password" required minLength={6} />
+        {!!errorMessage &&
+            <ErrorMessage>
+                {errorMessage}
+            </ErrorMessage>
+        }
         <Button type="submit" block>
             Register
         </Button>
